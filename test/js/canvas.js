8e2efb7ecc06870861708bfbe3a9ac001eb3ea34
lz_songlongfei <songlfneu@163.com>
2019-02-25 18:02:35 +0800

    edit

--- test/js/canvas.js
+++ test/js/canvas.js
@@ -1,30 +1,36 @@
 $(function () {
-  var canvas = document.getElementById('cvs');
-  context = canvas.getContext('2d');
+  var cvs = document.getElementById('cvs');
+  context = cvs.getContext('2d');
   function drawGrid(color, stepx, stepy) {
       context.save()
       context.fillStyle = 'white';
-      console.log(context);
       context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-      context.lineWidth = 0.1;
+      context.lineWidth = 1;
       context.strokeStyle = color;
       for (var i = stepx; i < context.canvas.width; i += stepx) {
           context.beginPath();
-          context.moveTo(i, 0);
-          context.lineTo(i, context.canvas.height);
+          context.moveTo(i + 0.5, 0);
+          context.lineTo(i + 0.5, context.canvas.height);
           context.closePath();
           context.stroke();
       }
       for (var j = stepy; j < context.canvas.height; j += stepy) {
           context.beginPath();
-          context.moveTo(0, j);
-          context.lineTo(context.canvas.width, j);
+          context.moveTo(0, j + 0.5);
+          context.lineTo(context.canvas.width, j + 0.5);
           context.closePath();
           context.stroke();
       }
       context.restore();
   }
-  drawGrid('#ccc', 20, 20);
+  drawGrid('#aaa', 20, 20);
 
+  $('#cvs').on('mousemove', function (e) {
+    console.log(e.offsetX)
+  })
+
+  context.fillStyle = '#ff0';
+  context.fillRect(21, 21, 19, 19);
+  context.fillRect(41, 41, 19, 19);
   
 })
